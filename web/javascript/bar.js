d107a1666d4b5a604d05bd0eff7f8d1ff932ce05
adarsh3699 <adarsh3699@gmail.com>
2022-03-07 21:38:19 +0530

    change locations

--- web/javascript/bar.js
+++ web/javascript/bar.js
@@ -1,12 +1,12 @@
-let logo = '<div id="logo"><a href="index.html"><img src="logo.png" height="50px"><div><span id="adarsh">Adarsh </span>Suman</div></a></div>';
+let logo = '<div id="logo"><a href="index.html"><img src="image/logo.png" height="50px"><div><span id="adarsh">Adarsh </span>Suman</div></a></div>';
 let desktopMenu = '<div id="desktopMenu">  <a href="index.html">Home</a> <a href="about.html">About</a> <a href="contact.html">Contact</a>  </div>';
-let social = '<div id="social"><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="insta.png" height="22px"></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="youtube.png" alt="Error" height="20px"></a></div>';
-let mobileIcon = '<div id="mobileIcon"><img src="menu.png" /></div>';
+let social = '<div id="social"><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="image/insta.png" height="22px"></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="image/youtube.png" height="20px"></a></div>';
+let mobileIcon = '<div id="mobileIcon"><img src="image/menu.png" /></div>';
 let mobileMenu = '<div id="mobileMenu"><a href="index.html">Home</a><a href="about.html">About</a><a href="contact.html">Contact</a></div>'
 
 let footMemu = '<div id="footMemu"><div><a href="index.html">Home</a></div><div><a href="about.html">About</a></div><div><a href="contact.html">Contact</a></div></div>'
-let footLogo ='<div id="footLogo"><img src="logo.png"><div>© 2022-25 Adarsh Creation</div><div>Developed by Adarsh Suman</div></div>'
-let footSocial ='<div id="footSocial"><a href="mailto:adarsh3699@gmail.com" target="_blank"><img src="google.png">adarsh3699<div></div></a><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="insta.png"  height="24px"><div>Instagram</div></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="youtube.png" height="23px"><div>&#160 YouTube</div></a></div>'
+let footLogo ='<div id="footLogo"><img src="image/logo.png"><div>© 2022-25 Adarsh Creation</div><div>Developed by Adarsh Suman</div></div>'
+let footSocial ='<div id="footSocial"><a href="mailto:adarsh3699@gmail.com" target="_blank"><img src="image/google.png">adarsh3699<div></div></a><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="image/insta.png"  height="24px"><div>Instagram</div></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="image/youtube.png" height="23px"><div>&#160 YouTube</div></a></div>'
 
 // document.getElementById('bar').innerHTML = '<div id="topBar">' + logo + desktopMenu + social + mobileIcon + '</div>' + mobileMenu;
 // document.getElementById('bottomBar').innerHTML = footMemu + footLogo + footSocial;
