53e3f93c2adb9453a800dff64dcbe3f1dca33e8d
adarsh3699 <adarsh3699@gmail.com>
2022-03-13 14:01:47 +0530

    beta 0.5

--- myWeb/javascript/bar.js
+++ myWeb/javascript/bar.js
@@ -5,11 +5,9 @@ let mobileIcon = '<div id="mobileIcon"><img src="image/menu.png" /></div>';
 let mobileMenu = '<div id="mobileMenu"><a href="index.html">Home</a><a href="about.html">About</a><a href="contact.html">Contact</a></div>'
 
 let footMemu = '<div id="footMemu"><div><a href="index.html">Home</a></div><div><a href="about.html">About</a></div><div><a href="contact.html">Contact</a></div></div>'
-let footLogo ='<div id="footLogo"><img src="image/logo.png"><div>© 2022-25 Adarsh Creation</div><div>Developed by Adarsh Suman</div></div>'
+let footLogo ='<div id="footLogo"><img src="image/logo.png"><div>© 2022-25 (Version 0.5)</div><div>Developed by Adarsh Suman</div></div>'
 let footSocial ='<div id="footSocial"><a href="mailto:adarsh3699@gmail.com" target="_blank"><img src="image/google.png">adarsh3699<div></div></a><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="image/insta.png"  height="24px"><div>Instagram</div></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="image/youtube.png" height="23px"><div>&#160 YouTube</div></a></div>'
 
-// document.getElementById('bar').innerHTML = '<div id="topBar">' + logo + desktopMenu + social + mobileIcon + '</div>' + mobileMenu;
-// document.getElementById('bottomBar').innerHTML = footMemu + footLogo + footSocial;
 
 $('#bar').html('<div id="topBar">' + logo + desktopMenu + social + mobileIcon + '</div>' + mobileMenu);
 $('#bottomBar').html(footMemu + footLogo + footSocial);
@@ -21,53 +19,34 @@ $('#mobileIcon').on("click", function(e) {
     flag = !flag;
     
     if (flag == true) {
-        // document.getElementById("mobileMenu").classList.add("show");
         $('#mobileMenu').addClass("show");
     } else {
-        // document.getElementById("mobileMenu").classList.remove("show");
         $('#mobileMenu').removeClass("show");
     }
 });
 
 $(document).on("click", function(e){
-    console.log("asdfg")
     $('#mobileMenu').removeClass("show");
 });
 
 $(document).ready(function(){
     $('#loaderBack').css('display', 'none');
-    console.log("loaded")
-    // setTimeout(function(){
-    //     $('#loaderBack').css('display', 'none');
-    // }, 500)
-});
-
-// $('#html').load('about.html');
-
-// let flag = true;
-// function handleMenuClick() {  
-//     flag = !flag;
     
-//     if (flag == true) {
-//         document.getElementById("mobileMenu").classList.add("show");
-//     } else {
-//         document.getElementById("mobileMenu").classList.remove("show");
-//     } 
-// }
+    $('.disclamer').remove();
+});
 
 $('#send').on("click", function() {
     let email = $('#email').val();
-    let text = $('.textArea').val();
+    let text = $('#textArea').val();
     
-        if (email === "") {
-            $("#confirmEmail").text('Please enter your email')
-        } else{
-            $("#confirmEmail").text('')
-        }
-        if (text === "") {
-            $('#confirmMsg').text('Please enter your msg')
-        } else {
-            $("#confirmMsg").text('')
-        }
-    
-})
+    if (email === "") {
+        $("#confirmEmail").text('Please enter your email')
+    } else{
+        $("#confirmEmail").text('')
+    }
+    if (text === "") {
+        $('#confirmMsg').text('Please enter your msg')
+    } else {
+        $("#confirmMsg").text('')
+    }
+});
