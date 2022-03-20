3fbcfbb016efcfd9d99e6c0e41380a18d4f8b322
adarsh3699 <adarsh3699@gmail.com>
2022-03-21 01:17:18 +0530

    create an acc

---
+++ login/api/createAcc.php
@@ -0,0 +1,43 @@
+<?php
+	include_once "dbConn.php";
+	$resp = array();
+
+    try {
+        //getting data from frontend sent in json format
+        $data = @json_decode(file_get_contents("php://input"), TRUE);
+
+        //if json decoded successfully
+        if (@json_last_error() == JSON_ERROR_NONE) {
+            if (@is_array($data)) {
+                //our logic
+                $userName = $data["userName"];
+                $password = $data["password"];
+                if ($userName != "") {
+                    $query = "INSERT INTO userSearch (userName, password) VALUES ('$userName', '$password');";
+                    if ($queryRun = @mysqli_query($dbLink, $query)) {
+                        $resp["statusCode"] = 200;
+                        $resp["msg"] = "inserted";
+                    } else {
+                        $resp["statusCode"] = 400; //bad request
+                        $resp["msg"] = "Bad request";
+                    }
+                } else {
+                    $resp["statusCode"] = 400; //bad request
+                    $resp["msg"] = "Bad request"; 
+                }
+                //our logic
+            } else {
+                $resp["statusCode"] = 500; //Internal server error
+                $resp["msg"] = "Something went wrong";
+            }
+        } else {
+            $resp["statusCode"] = 500; //Internal server error
+            $resp["msg"] = "Something went wrong";
+        }
+    } catch (Throwable $e) {
+        $resp["statusCode"] = 500; //bad request
+        $resp["msg"] = "Something went wrong " . $e->getMessage();
+    }
+
+	print_r(@json_encode($resp));
+?>
