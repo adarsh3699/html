02661d24720ba33ed39c2fe5f43e13b4bd6840a1
adarsh3699 <adarsh3699@gmail.com>
2022-03-22 01:07:46 +0530

    all most done

--- login/bhemu.js
+++ login/bhemu.js
@@ -17,11 +17,29 @@ $("#login").on("click", function() {
     let userName = $("#userName").val();
     let password = $("#password").val();
 
-        postApiCall("http://localhost/html/login/api/userSearch.php", {userName: userName, password: password}, function(resp){
+    postApiCall("http://localhost/html/login/api/userSearch.php", {userName: userName, password: password}, function(resp){
+        if(resp.statusCode === 200) {
+            $("#msg").text(resp.msg);
+        } else{
+            $("#msg").text(resp.msg);
+        }
+    })
+})
+
+$("#signup").on("click", function() {
+    let userName = $("#newUserName").val();
+    let password = $("#newPassword").val();
+    let confirmPass = $("#confirmPass").val();
+    
+    if(password == confirmPass) {
+        postApiCall("http://localhost/html/login/api/createAcc.php", {userName: userName, password: password}, function(resp){
             if(resp.statusCode === 200) {
-                $("#msg").text(resp.msg);
+                $("#updateMsg").text(resp.msg);
             } else{
-                $("#msg").text(resp.msg);
+                $("#updateMsg").text(resp.msg);
             }
         })
+    } else {
+        $("#updateMsg").text("Passwords didn't match. Try again.");
+    }
 })
