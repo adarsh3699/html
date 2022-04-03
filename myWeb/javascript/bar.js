f8c3b3ff334a88170b7c6c955e51c612b7b50fb8
adarsh3699 <adarsh3699@gmail.com>
2022-04-03 23:05:52 +0530

    my web hide html

--- myWeb/javascript/bar.js
+++ myWeb/javascript/bar.js
@@ -1,12 +1,20 @@
-let logo = '<div id="logo"><a href="index.html"><img src="image/logo.png" height="50px"><div><span id="adarsh">Adarsh </span>Suman</div></a></div>';
-let desktopMenu = '<div id="desktopMenu">  <a href="index.html">Home</a> <a href="about.html">About</a> <a href="contact.html">Contact</a>  </div>';
-let social = '<div id="social"><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="image/insta.png" height="22px"></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="image/youtube.png" height="20px"></a></div>';
-let mobileIcon = '<div id="mobileIcon"><img src="image/menu.png" /></div>';
-let mobileMenu = '<div id="mobileMenu"><a href="index.html">Home</a><a href="about.html">About</a><a href="contact.html">Contact</a></div>'
+const inFolder = $('#bar').attr("inFolder");
+let back = "";
 
-let footMemu = '<div id="footMemu"><div><a href="index.html">Home</a></div><div><a href="about.html">About</a></div><div><a href="contact.html">Contact</a></div></div>'
-let footLogo ='<div id="footLogo"><img src="image/logo.png"><div>© 2022-25 (V 0.9)</div><div>Developed by Adarsh Suman</div></div>'
-let footSocial ='<div id="footSocial"><a href="mailto:adarsh3699@gmail.com" target="_blank"><img src="image/google.png" height = "22px">adarsh3699<div></div></a><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="image/insta.png"  height="19px"><div>Instagram</div></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="image/youtube.png" height="18px"><div>&#160 YouTube</div></a></div>'
+if (inFolder == "yes") {
+    back = "../";
+}
+console.log(back);
+
+let logo = '<div id="logo"><a href="'+ back +'"><img src="'+ back +'image/logo.png" height="50px"><div><span id="adarsh">Adarsh </span>Suman</div></a></div>';
+let desktopMenu = '<div id="desktopMenu"> <a href="'+ back +'">Home</a> <a href="'+ back +'about/">About</a> <a href="'+ back +'contact/">Contact</a>  </div>';
+let social = '<div id="social"><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="'+ back +'image/insta.png" height="22px"></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="'+ back +'image/youtube.png" height="20px"></a></div>';
+let mobileIcon = '<div id="mobileIcon"><img src="'+ back +'image/menu.png" /></div>';
+let mobileMenu = '<div id="mobileMenu"> <a href="'+ back +'">Home</a> <a href="'+ back +'about/">About</a> <a href="'+ back +'contact/">Contact</a>  </div>';
+
+let footMemu = '<div id="footMemu"><div><a href="'+ back +'">Home</a></div><div><a href="'+ back +'about/">About</a></div><div><a href="'+ back +'contact">Contact</a></div></div>'
+let footLogo ='<div id="footLogo"><img src="'+ back +'image/logo.png"><div>© 2022-25 (V 0.9)</div><div>Developed by Adarsh Suman</div></div>'
+let footSocial ='<div id="footSocial"><a href="mailto:adarsh3699@gmail.com" target="_blank"><img src="'+ back +'image/google.png" height = "22px">adarsh3699<div></div></a><a href="https://www.instagram.com/_adarsh.s/" target="_blank"><img src="'+ back +'image/insta.png"  height="19px"><div>Instagram</div></a><a href="https://www.youtube.com/channel/UCkdSVbjY4sS1I7hw9ZJMdew" target="_blank"><img src="'+ back +'image/youtube.png" height="18px"><div>&#160 YouTube</div></a></div>'
 
 
 $('#bar').html('<div id="topBar">' + logo + desktopMenu + social + mobileIcon + '</div>' + mobileMenu);
