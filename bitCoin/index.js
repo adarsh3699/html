21dbabf83587632819a32ea001a7a2274622258f
adarsh3699 <adarsh3699@gmail.com>
2022-08-29 14:38:29 +0530

    push

--- bitCoin/index.js
+++ bitCoin/index.js
@@ -8,20 +8,9 @@ async function apiCall(link, functionCall) {
     }
 }
 
-// let gitApiLink = "https://api.github.com/users/adarsh3699";
-
-// function renderDataFromGit(data) {
-//     $("#load").remove();
-//     $("#avatar").attr("src", data.avatar_url)
-//     $("#name").text(data.name);
-//     $("#follow")body.html("<div> followers :- " + data.followers + "</div> <div> following :- " + data.following + "</div>");
-// }
-
-// apiCall(gitApiLink, renderDataFromGit);
-
 apiCall("https://api.coindesk.com/v1/bpi/currentprice.json", function(data) {
     $("#load").remove();
-    $("#time").text("time:- " + data?.time?.updated)
+    $("#time").text("Time:- " + data?.time?.updated)
   
     const bpi = Object.keys(data.bpi);
     for(let i = 0; i < bpi.length; i++) {
@@ -32,19 +21,3 @@ apiCall("https://api.coindesk.com/v1/bpi/currentprice.json", function(data) {
         $("#rate").text(data?.bpi?.[curr]?.rate);
     })
 });
-
-for (let i = 1; i <= 50; i++) {
-    $("#count").append("<div>" + i + "</div>")
-}
-$("#count div").on("click", function() {
-    $(this).nextAll().remove();
-});
-
-$(window).on("scroll", function(e) {
-    console.log("scroll", this.scrollY)
-    if (this.scrollY >= 0) {
-        // e.preventDefault();
-        // return false;
-        window.scrollTo(this.scrollX, 0)
-    }
-})
